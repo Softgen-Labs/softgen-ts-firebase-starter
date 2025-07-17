7a0b703b17b0aa5cdea85e00ed2071ef0346a9f2
Artem Kozaev <artemkozaev@gmail.com>
2025-07-17 22:22:30 +0300

    more permissive linter rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,6 +16,9 @@ const eslintConfig = [
   {
     rules: {
       "react/no-unescaped-entities": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-non-null-assertion": "warn",
     },
   },
 ];
