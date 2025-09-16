1ef4ca1dcab2229522560c84b0bd553c0ee5bc3b
sutharjay1 <sutharjay3635@gmail.com>
2025-09-16 13:59:35 +0530

    fix(app): remove imp

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -14,7 +14,7 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
         outline:
-          "border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50 !border !border-border !bg-transparent !text-black dark:!text-white !hover:bg-accent ",
+          "border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50 !border !border-border !bg-transparent text-black dark:text-white !hover:bg-accent ",
         secondary:
           "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
